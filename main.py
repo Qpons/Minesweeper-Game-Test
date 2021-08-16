86c4fa9b53109131eb7d39f7725ecce7f1de7b5e
Calvin Wu <calvinwu0716@gmail.com>
2021-08-16 10:16:16 -0700

    Added error checking and button animation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from random import randrange
+from tkinter import messagebox
 
 root = Tk()
 
@@ -7,25 +9,29 @@ y_axis_size = 0
 mine_count = 0
 minefield_btn_list = []
 
-top_frame = Frame(root)
-top_frame.grid(row=0, column=0)
+setup_frame = Frame(root)
+setup_frame.grid(row=0, column=0)
 
 mine_frame = Frame(root)
 mine_frame.grid(row=1, column=0)
 
-Label(top_frame, text='Minefield size').grid(row=0,column=1, pady= 5)
-x_axis_entry = Entry(top_frame, width=6)
+Label(setup_frame, text='Minefield size').grid(row=0,column=1, pady= 5)
+x_axis_entry = Entry(setup_frame, width=6)
 x_axis_entry.grid(row=1, column=0, pady= 5)
-Label(top_frame, text=' x ').grid(row=1, column=1, pady= 5)
-y_axis_entry = Entry(top_frame, width=6)
+Label(setup_frame, text='(1-20)').grid(row=2,column=0)
+Label(setup_frame, text=' x ').grid(row=1, column=1, pady= 5)
+y_axis_entry = Entry(setup_frame, width=6)
 y_axis_entry.grid(row=1, column=2, pady= 5)
+Label(setup_frame, text='(1-20)').grid(row=2,column=2)
 
-Label(top_frame, text= 'Mine Count').grid(row=2,column=1, pady=5)
-mine_count_entry = Entry(top_frame, width=6)
-mine_count_entry.grid(row=3, column=1, pady= 5)
+Label(setup_frame, text= 'Mine Count').grid(row=3,column=1, pady=5)
+mine_count_entry = Entry(setup_frame, width=6)
+mine_count_entry.grid(row=4, column=1, pady= 5)
+Label(setup_frame, text='(1-20)').grid(row=5,column=1)
 
-Button(top_frame, text='Start', command=lambda:build_minefield()).grid(row=4,column=1, pady=5)
+Button(setup_frame, text='Start', command=lambda:build_minefield()).grid(row=6,column=1, pady=5)
 
+#Build minefield buttons based on user entry in the status section
 def build_minefield():
     global minefield_btn_list
     global mine_count
@@ -33,14 +39,23 @@ def build_minefield():
     print(x_axis_entry.get() + 'x' + y_axis_entry.get())
     row_size = int(x_axis_entry.get())
     column_size = int(y_axis_entry.get())
-    mine_count = mine_count_entry.get()
-    minefield_btn_list = []
-    for i in range(column_size):
-        temp_row_list = []
-        for j in range(row_size):
-            mine_btn = Button(mine_frame, width=2)
-            mine_btn.grid(row=i, column=j)
-            temp_row_list.append(mine_btn)
-        minefield_btn_list.append(temp_row_list)
+    mine_count = int(mine_count_entry.get())
+    if (0 >= row_size > 20) or (0 >= column_size > 20) or (0 >= mine_count > 20):
+        print('Error')
+        messagebox.showerror('Error!', 'Mine Count and Minefield Size Out of Range!')
+    else:
+        minefield_btn_list = []
+        for i in range(column_size):
+            temp_row_list = []
+            for j in range(row_size):
+                mine_btn = Button(mine_frame, command=lambda m=i, n=j:mine_check(m,n), width=2)
+                mine_btn.grid(row=i, column=j)
+                temp_row_list.append(mine_btn)
+            minefield_btn_list.append(temp_row_list)
+
+def mine_check(mine_row, mine_column):
+    print(mine_row)
+    print(mine_column)
+    minefield_btn_list[mine_row][mine_column].config(relief=SUNKEN)  
 
 mainloop()

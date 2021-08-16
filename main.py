7afaa3f002e4160e2fbc99b3f7f870a1db3d12f6
Calvin Wu <calvinwu0716@gmail.com>
2021-08-16 10:50:33 -0700

    Restart functionality improved
    Destroy previous frame and rebuild minefield.
    Added color and text to buttons when pressed

--- main.py
+++ main.py
@@ -30,14 +30,20 @@ mine_count_entry = Entry(setup_frame, width=6)
 mine_count_entry.grid(row=4, column=1, pady= 5)
 Label(setup_frame, text='(1-20)').grid(row=5,column=1)
 
-Button(setup_frame, text='Start', command=lambda:build_minefield()).grid(row=6,column=1, pady=5)
+start_btn = Button(setup_frame, text='Start', command=lambda:build_minefield())
+start_btn.grid(row=6,column=1, pady=5)
 
 #Build minefield buttons based on user entry in the status section
 def build_minefield():
     global minefield_btn_list
     global minefield_mine_list
     global mine_count
-    print(mine_count)
+    global mine_frame
+    mine_frame.destroy()
+    mine_frame = Frame(root)
+    mine_frame.grid(row=1, column=0)
+    start_btn['text'] = 'Restart'
+    print(mine_count_entry.get())
     print(x_axis_entry.get() + 'x' + y_axis_entry.get())
     row_size = int(x_axis_entry.get())
     column_size = int(y_axis_entry.get())
@@ -54,7 +60,7 @@ def build_minefield():
                 mine_btn = Button(mine_frame, command=lambda m=i, n=j:mine_check(m,n), width=2)
                 mine_btn.grid(row=i, column=j)
                 temp_row_list.append(mine_btn)
-                is_mine = IntVar()
+                is_mine = 0
                 temp_mine_list.append(is_mine)
             minefield_btn_list.append(temp_row_list)
             minefield_mine_list.append(temp_mine_list)
@@ -71,8 +77,15 @@ def build_minefield():
 def mine_check(mine_row, mine_column):
     print(mine_row)
     print(mine_column)
-    if minefield_mine_list[mine_row][mine_column] == 1:
-        messagebox.showerror('Game Over!', 'Stepped on a Mine :(')
     minefield_btn_list[mine_row][mine_column].config(relief=SUNKEN)  
-
+    if minefield_mine_list[mine_row][mine_column] == 1:
+        minefield_btn_list[mine_row][mine_column]['text'] = ':('
+        minefield_btn_list[mine_row][mine_column]['bg'] = 'red'
+        minefield_btn_list[mine_row][mine_column]['fg'] = 'white'
+        messagebox.showerror('Game Over!', 'Stepped on a Mine :(')
+    else:
+        minefield_btn_list[mine_row][mine_column]['text'] = ':D'
+        minefield_btn_list[mine_row][mine_column]['bg'] = 'green'
+        minefield_btn_list[mine_row][mine_column]['fg'] = 'white'
+    
 mainloop()
